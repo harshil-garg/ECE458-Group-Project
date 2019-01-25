1c73a40cf3849774db5525fb529cc37cd9ffef5c
Juan Philippe <jap87@duke.edu>
2019-01-25 18:52:13 -0500

    finished register user basic feature

--- angular-src/src/app/register/register.component.ts
+++ angular-src/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-register',
@@ -6,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+	registerError = false;
+	successfulRegistration = false;
 
-	constructor() { }
+	constructor(private accountsService: AccountsService) { }
 
 	ngOnInit() { }
 
 	register(name: string, email: string, password: string, password2: string) {
-		
+		this.accountsService.register(name, email, password, password2).subscribe((response) => {
+			this.successfulRegistration = response.success;
+			if (!response.success) {
+				this.registerError = true;
+			}
+		}, (err) => {
+			this.registerError = true;
+		});
 	}
 
 }
