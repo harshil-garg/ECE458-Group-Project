b12912231a43703ddf3ab361f6c403294d9467f5
Juan Philippe <jap87@duke.edu>
2019-02-03 14:35:44 -0500

    multiple files simultaneously

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -12,24 +12,28 @@ let uploadSession = {
   started: false
 }
 
-router.post('/', upload.single('file'), function (req, res) {
+router.post('/', upload.array('file[]', 4), function (req, res) {
     uploadSession.started = true;
-    const fileRows = [];
+    
   
     // open uploaded file
-    csv.fromPath(req.file.path)
+    req.files.forEach(file => {
+      const fileRows = [];
+      csv.fromPath(file.path, {headers: true})
       .on("data", function (data) {
         fileRows.push(data); // push each row
       })
       .on("end", function () {
         console.log(fileRows);
-        console.log('HELLO WORLD');
-        fs.unlinkSync(req.file.path);   // remove temp file
-        //process "fileRows" and respond
+        fs.unlinkSync(file.path);   // remove temp file
       })
+    })
 
-      uploadSession.started = false;
       res.json({success: true, errorList: []});
   });
+  //This will be called when we have an array of files.
+  function handleFiles() {
+
+  }
 
   module.exports = router;
