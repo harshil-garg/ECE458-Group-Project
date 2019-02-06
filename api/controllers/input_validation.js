5dc19ce54dc429c0982c447692da37dfaef70fdd
Jesse Yue <someonebob156@gmail.com>
2019-02-06 03:36:04 -0500

    check if inputs are alphanumeric

--- api/controllers/input_validation.js
+++ api/controllers/input_validation.js
@@ -1,8 +1,11 @@
 module.exports.passed = function(params, res){
     errors = []
     for(let key of Object.keys(params)){
+        let alphanumeric = new RegExp(/^[a-zA-Z0-9]*$/);
         if(!params[key]){
             errors.push(`Please fill in ${key}`);
+        }else if(!params[key].toString().match(alphanumeric)){
+            errors.push(`${key} is not alphanumeric`);
         }
     }
 
