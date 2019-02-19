65aead3341333edb9096a98f1a5eaa1068850063
Jesse Yue <someonebob156@gmail.com>
2019-02-19 16:41:54 -0500

    add limit to autocomplete

--- api/controllers/autocomplete.js
+++ api/controllers/autocomplete.js
@@ -1,12 +1,13 @@
+const limit = 10;
 module.exports.nameOrNumber = async (model, input) => {
     let regex = new RegExp('^'+input, 'i');
     if(isNaN(input)){
-        let results = await model.find({name: regex}).limit(10).collation({locale: 'en'}).sort('name').lean().exec();
+        let results = await model.find({name: regex}).limit(limit).collation({locale: 'en'}).sort('name').lean().exec();
         return results;
     }else{
         //tolower converts number to string, 1 means to include
         let results = [];
-        let cursor = model.aggregate({$addFields: {num2str: {'$toLower' : '$number'}, name: 1, size: 1, count: 1}}).match({num2str: regex}).cursor({}).exec();
+        let cursor = model.aggregate({$addFields: {num2str: {'$toLower' : '$number'}}}).match({num2str: regex}).limit(limit).cursor({}).exec();
         await cursor.eachAsync((sku) => {
             results.push(sku);
         });
