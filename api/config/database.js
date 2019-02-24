d4033ec908e51cdb81d5b3d2edd5fba2503d6140
Juan Philippe <jap87@duke.edu>
2019-02-24 12:00:56 -0500

    revert mongo url

--- api/config/database.js
+++ api/config/database.js
@@ -1,4 +1,4 @@
 // 27017 is the default port number.
 module.exports = {
-    database: 'mongodb://127.0.0.1:27017/hypomeal'
+    database: 'mongodb://admin:admin458@ds155203.mlab.com:55203/hypotheticalmeals'
 }
