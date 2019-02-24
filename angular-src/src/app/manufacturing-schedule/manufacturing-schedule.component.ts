32af7b1cd3164b8aa1d37d861ac246a592b9bfdb
Jimmy Shackford <jas199@duke.edu>
2019-02-23 22:28:47 -0500

    fixed reordering animation problems with schedule

--- angular-src/src/app/manufacturing-schedule/manufacturing-schedule.component.ts
+++ angular-src/src/app/manufacturing-schedule/manufacturing-schedule.component.ts
@@ -14,6 +14,7 @@ export class ManufacturingScheduleComponent implements OnInit {
   activities : Array<ManufacturingActivity> = [];
   hourHeaders : Array<string> = [];
   manufLines : Array<string> = [];
+  currDate : Date = new Date();
 
   constructor() { }
 
@@ -22,12 +23,27 @@ export class ManufacturingScheduleComponent implements OnInit {
     this.manufLines.push("Line2");
     this.manufLines.push("Line3");
     var newDate = new Date();
+    newDate.setHours(12);
     this.activities.push({
       activity: "beats",
       manufacturing_line: "Line1",
       start_date: newDate,
       duration: 3
-    })
+    });
+    var newDate2 = new Date();
+    newDate2.setHours(8);
+    this.activities.push({
+      activity: "as",
+      manufacturing_line: "Line1",
+      start_date: newDate2,
+      duration: 3
+    });
+    this.setupHourHeaders();
+    this.refreshHours();
+    console.log("REFRESHED");
+  }
+
+  setupHourHeaders(){
     for(var j=0; j<10; j++){
       if(j<4){
         this.hourHeaders.push((j+8) + "AM");
@@ -39,27 +55,48 @@ export class ManufacturingScheduleComponent implements OnInit {
         this.hourHeaders.push((j-4) + "PM");
       }
     }
+  }
+
+  refreshHours(){
     for(var i=0; i<this.manufLines.length; i++){
       this.hours[i] = [];
       for(var j=0; j<10; j++){
         this.hours[i].push("");
       }
     }
-    this.hours[0][1] = "hello world";
-    this.hours[2][5] = "sup";
+    for(var i=0; i<this.activities.length; i++){//iterate over activities
+      var currDate = new Date();
+      for(var j=0; j<10; j++){//iterate over hours
+        currDate.setHours(j+8);
+        var currTime = currDate.getTime();
+        var startTime = this.activities[i].start_date.getTime();
+        var endTime = this.activities[i].start_date.getTime() + this.convertToMillis(this.activities[i].duration,0,0,0);
+        if(currTime>=startTime && currTime<=endTime){
+          this.hours[0][j] = this.activities[i].activity;
+        }
+      }
+    }
+  }
+
+  convertToMillis(hour,minute,second,millis){
+    var minutes = minute + hour*60;
+    var second = second + minutes*60;
+    return millis + second*1000;
   }
 
   drop(event: CdkDragDrop<string[]>) {
     if(event.previousContainer.id === "manufacturing-activities"){
-      var currId  = event.container.id.substring(5);//still a string, need to convert to int with unary operator (+)
+      var currId  = event.container.id.split("-")[1];//still a string, need to convert to int with unary operator (+)
       this.hours[+currId][event.currentIndex] = event.previousContainer.data[event.previousIndex];
     }
     else {
-      var prevId = event.previousContainer.id.substring(5);
-      var currId = event.container.id.substring(5);
-      var initialValue = this.hours[prevId][event.previousIndex];
-      this.hours[+prevId][event.previousIndex] = "";
-      this.hours[+currId][event.currentIndex] = initialValue;
+      var prevId = event.previousContainer.id.split("-")[1];
+      var prevHour = event.previousContainer.id.split("-")[3];
+      var currId = event.container.id.split("-")[1];
+      var currHour = event.container.id.split("-")[3];
+      var initialValue = this.hours[+prevId][prevHour];
+      this.hours[+prevId][prevHour] = "";
+      this.hours[+currId][currHour] = initialValue;
     }
   }
 
@@ -76,7 +113,9 @@ export class ManufacturingScheduleComponent implements OnInit {
   getLists() {
     var lists = [];
     for(var i=0; i<this.manufLines.length; i++){
-      lists.push("list-"+i);
+      for(var j=0; j<10; j++) {
+        lists.push("list-"+i+"-hour-"+j);
+      }
     }
     return lists;
   }
