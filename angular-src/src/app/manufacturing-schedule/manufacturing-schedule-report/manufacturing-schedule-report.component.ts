81d3d7618cade188e014b6c7518ef528071865cf
VCM admin account <vcm@vcm-8670.vm.duke.edu>
2019-02-27 04:20:03 -0500

    pdf shit like juan said is working

--- angular-src/src/app/manufacturing-schedule/manufacturing-schedule-report/manufacturing-schedule-report.component.ts
+++ angular-src/src/app/manufacturing-schedule/manufacturing-schedule-report/manufacturing-schedule-report.component.ts
@@ -41,6 +41,92 @@ export class ManufacturingScheduleReportComponent implements OnInit {
       });
     }
 
+    print() {
+      let w = window.open();
+      w.document.head.innerHTML += '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">';
+      w.document.head.innerHTML += '<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>';
+      w.document.head.innerHTML += '<style>th {text-align: center} table {font-size: 12px} .table > tbody > tr > td {vertical-align: middle;}</style>'
+      let htmlString = `
+      <h1>Manufacturing Schedule Report</h1>
+      <div id="table" class="table-editable">
+      <table class="table table-bordered table-responsive-md table-striped text-center">
+        <tr>
+          <th class="text-center">Activity ID</th>
+          <th class="text-center">SKU Display</th>
+          <th class="text-center">SKU ID</th>
+          <th class="text-center">SKU Case Quantity</th>
+          <th class="text-center">Formula ID</th>
+          <th class="text-center">Formula Name</th>
+          <th class="text-center">Formula / Ingredients</th>
+          <th class="text-center">Start</th>
+          <th class="text-center">End</th>
+          <th class="text-center">Duration</th>
+        </tr>
+        `;
+        var id=0;
+        for (let activity of this.activities) {
+          htmlString += `
+          <tr>
+          <td contenteditable="false">${id}</td>
+          <td contenteditable="false">${activity.sku_display_name}</td>
+          <td contenteditable="false">${activity.sku_id}</td>
+          <td contenteditable="false">${activity.sku_case_quantity}</td>
+          <td contenteditable="false">${activity.formula_id}</td>
+          <td contenteditable="false">${activity.formula_name}</td>
+          <td>
+                  <table class="table table-bordered">
+                      <tr>
+                          <th class="text-center">Name</th>
+                          <th class="text-center">ID</th>
+                          <th class="text-center">Quantity/Units</th>
+                        </tr>
+          `;
+          for (let tuple of activity.formula_ingredient_tuples) {
+            htmlString += `
+            <tr>
+                              <td>${tuple.ingredient.name}</td>
+                              <td>${tuple.ingredient.number}</td>
+                              <td>${tuple.quantity}${tuple.unit}</td>
+                            </tr>
+            `;
+          }
+          htmlString += `
+          </table>
+              </td>
+              <td contenteditable="false">${activity.start_date}</td>
+              <td contenteditable="false">${activity.end_date}</td>
+              <td contenteditable="false">${activity.duration}</td>
+            </tr>
+          `;
+          id = id + 1;
+        }
+        htmlString += `</table></div>`;
+
+        // Now for the second table
+        htmlString +=  `
+        <div id="table2" class="table-editable">
+          <table class="table table-bordered table-responsive-md table-striped text-center">
+              <tr>
+                  <th class="text-center">Ingredient Name</th>
+                  <th class="text-center">Required Amount</th>
+                  <th class="text-center">Unit</th>
+                  <th class="text-center">Required Num Packages</th>
+                </tr>`;
+        for (let ing of this.summationData) {
+          htmlString += `
+          <tr>
+                    <td contenteditable="false">${ing.name}</td>
+                    <td contenteditable="false">${ing.unit_value}</td>
+                    <td contenteditable="false">${ing.unit}</td>
+                    <td contenteditable="false">${ing.package_value}</td>
+                  </tr>
+          `;
+        }
+        htmlString += `</table></div>`;         
+          
+        w.document.body.innerHTML = htmlString;
+    }
+
     onNoClick(): void {
       this.dialogRef.close();
     }
