2d93fdf26022f5aa88aed2294bce3a0b5938f491
someonebob <vcm@vcm-9053.vm.duke.edu>
2019-03-22 02:35:25 -0400

    finished table layout for drilldown

--- angular-src/src/app/sales-report/sales-drilldown/sales-drilldown.component.ts
+++ angular-src/src/app/sales-report/sales-drilldown/sales-drilldown.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
+import { MatTableDataSource, MatSnackBar, MatSort } from '@angular/material';
 import { Sku } from '../../model/sku';
+import { SalesRecord } from '../../model/sales-record';
 
 @Component({
   selector: 'app-sales-drilldown',
@@ -7,21 +9,43 @@ import { Sku } from '../../model/sku';
   styleUrls: ['./sales-drilldown.component.css']
 })
 export class SalesDrilldownComponent implements OnInit {
-  sku
+  recordList: Array<SalesRecord> = [];
+  sku: any;
   // @Input() sku : Sku
   display_name: string;
+  displayedColumns: string[] = ['year', 'week', 'customer number', 'customer name', 'sales', 'price', 'revenue'];
+  dataSource = new MatTableDataSource<SalesRecord>(this.recordList)
 
   constructor() { }
 
   ngOnInit() {
     this.sku = {
-      name: "hello",
-      id: 1,
-      unit_size: "5",
+      name: "Burger_Beef",
+      id: 1001,
+      unit_size: "5 pounds",
       count_per_case: 30
     }
-    
-    this.display_name = `${this.sku.name} : ${this.sku.unit_size} * ${this.sku.count_per_case} (${this.sku.id})`
+
+    this.display_name = `${this.sku.name} : ${this.sku.unit_size} * ${this.sku.count_per_case} (${this.sku.id})`;
+    for(let i = 0; i < 10; i++){
+      this.recordList.push({
+        year: 2010+i,
+        week: 5,
+        customer_number: 1+i,
+        customer_name: `poop${i}`,
+        sales: 10*i,
+        price: 20*i,
+        revenue: 30*i
+      })
+    }
+  }
+
+  getTotal(key: string){
+    let total = 0;
+    for(let record of this.recordList){
+      total += record[key]
+    }
+    return total;
   }
 
 }
