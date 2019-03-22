f2dadd3d019c375c22b9b27f97e3a4db8590a184
someonebob <vcm@vcm-9053.vm.duke.edu>
2019-03-22 01:07:30 -0400

    started layout for drilldown

--- angular-src/src/app/sales-report/sales-drilldown/sales-drilldown.component.ts
+++ angular-src/src/app/sales-report/sales-drilldown/sales-drilldown.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { Sku } from '../../model/sku';
 
 @Component({
   selector: 'app-sales-drilldown',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-drilldown.component.css']
 })
 export class SalesDrilldownComponent implements OnInit {
+  sku
+  // @Input() sku : Sku
+  display_name: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.sku = {
+      name: "hello",
+      id: 1,
+      unit_size: "5",
+      count_per_case: 30
+    }
+    
+    this.display_name = `${this.sku.name} : ${this.sku.unit_size} * ${this.sku.count_per_case} (${this.sku.id})`
   }
 
 }
