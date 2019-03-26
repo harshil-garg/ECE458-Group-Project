8d04d952ca6cfacc7df79f88d48ba46daccb48c6
VCM admin account <vcm@vcm-8825.vm.duke.edu>
2019-03-26 12:29:11 -0400

    dependency report shows refresh animation when loading

--- angular-src/src/app/ingredients-table/create-dependency-report/create-dependency-report.component.ts
+++ angular-src/src/app/ingredients-table/create-dependency-report/create-dependency-report.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import { DependencyReportDialogComponent } from './dependency-report-dialog/dependency-report-dialog.component';
-import { FilterIngredientsService } from '../filter-ingredients.service'
 
 @Component({
   selector: 'app-create-dependency-report',
@@ -10,53 +9,18 @@ import { FilterIngredientsService } from '../filter-ingredients.service'
 })
 export class CreateDependencyReportComponent{
 
-  constructor(public dialog: MatDialog, public filterIngredientsService: FilterIngredientsService) {}
+  constructor(public dialog: MatDialog) {}
 
   @Input() sortBy: string;
   @Input() keywords: Array<any>;
   @Input() skus: Array<any>;
 
-  ingredients: Array<any>;
-
   onclick(){
-    this.filterIngredientsService.filter({
-        sortBy : this.sortBy,
-        pageNum: '-1',
-        page_size: 0,
-        keywords: this.keywords,
-        skus : this.skus
-      }).subscribe(
-      response => this.handleRefreshResponse(response),
-      err => {
-        if (err.status === 401) {
-          console.log("401 Error")
-        }
-      }
-    );
-  }
-
-  handleRefreshResponse(response){
-    console.log("RESPONSE:");
-    console.log(response);
-    if(response.success){
-      this.ingredients = [];
-      for(let ingredient of response.data){
-        this.ingredients.push({
-            id: ingredient.number,
-            name: ingredient.name,
-            vendor_info: ingredient.vendor_info,
-            package_size: ingredient.package_size,
-            cost_per_package: ingredient.cost,
-            skus: ingredient.skus,
-            comment: ingredient.comment
-        });
-      }
-      let dialogRef = this.dialog.open(DependencyReportDialogComponent, {
-        height: '800px',
-        width: '1400px',
-        data: this.ingredients
-      });
-    }
+    let dialogRef = this.dialog.open(DependencyReportDialogComponent, {
+      height: '800px',
+      width: '1400px',
+      data: {sortBy: this.sortBy, keywords: this.keywords, skus: this.skus}
+    });
   }
 
 }
